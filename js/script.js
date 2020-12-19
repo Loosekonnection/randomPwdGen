d02ac8058f25fd85e2afea6cb6368950c2115b3e
Loosekonnection <david.bates@loosekonnection.co.uk>
2020-12-19 20:08:08 +0000

    JS Code Refactor

--- js/script.js
+++ js/script.js
@@ -3,11 +3,10 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-
+    passwordText.value = password;
 }
 
 // Add event listener to generate button
@@ -15,88 +14,69 @@ generateBtn.addEventListener("click", writePassword);
 
 // Object to Generate Random Characters using respective functions
 var genRanFunctions = {
-  numChoice: "genRanNum()",
-  lcChoice: "genRanLc()",
-  ucChoice: "genRanUc()",
-  splChoice: "genRanSpl()"
+    numChoice: "genRanNum()",
+    lcChoice: "genRanLc()",
+    ucChoice: "genRanUc()",
+    splChoice: "genRanSpl()"
 };
 
-// Generate Random Numbers Function
+// Functions that generate random characters
 function genRanNum() {
-  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+    return (Math.floor(Math.random() * 10)).toString();
 }
 
-// Generate Random Lowercase Letters Function
 function genRanLc() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
 
-// Generate Random Uppercase Letters Function
 function genRanUc() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 }
 
-// Generate Random Special Characters Function
 function genRanSpl() {
-  var symbols = ['!', '\"', '£', '$', '%', '^', '&', '*', '>', '<', '\;', '(', ')', '+', '.', ',', '-', ':', '?', '[', ']', '_', '{', '}', '@', '\/', '\\',];
-  return symbols[Math.floor(Math.random() * symbols.length)];
+    var symbols = ['!', '\"', '£', '$', '%', '^', '&', '*', '>', '<', '\;', '(', ')', '+', '.', ',', '-', ':', '?', '[', ']', '_', '{', '}', '@', '\/', '\\',];
+    return symbols[Math.floor(Math.random() * symbols.length)];
 }
 
 // User Prompts to generate a password from
 function generatePassword() {
-  pwdLength = parseInt(prompt("Please choose a password length between 8 and 128 Characters long"));
-  if (pwdLength >= 8 && pwdLength <= 128) {
+    pwdLength = parseInt(prompt("Please choose a password length between 8 and 128 Characters long"));
+    if (pwdLength >= 8 && pwdLength <= 128) {
 
-    var numChoice = confirm("Would you like the password to contain: Numbers?");
-    var lcChoice = confirm("Would you like the password to contain: Lowercase Letters?");
-    var ucChoice = confirm("Would you like the password to contain: Uppercase Letters?");
-    var splChoice = confirm("Would you like the password to contain: Special Characters?");
-    //console.log(typeof pwdLength, pwdLength, numChoice, lcChoice, ucChoice, splChoice);
+        var numChoice = confirm("Would you like the password to contain: Numbers?");
+        var lcChoice = confirm("Would you like the password to contain: Lowercase Letters?");
+        var ucChoice = confirm("Would you like the password to contain: Uppercase Letters?");
+        var splChoice = confirm("Would you like the password to contain: Special Characters?");
 
-    // calculating the boolean true choices, so we can break out if none are chosen
-    var countChoices = numChoice + lcChoice + ucChoice + splChoice;
-    //console.log(countChoices);
+        // Array of Objects containing the Choices and then filtering out false choices
+        var choicesArr = [{ numChoice }, { lcChoice }, { ucChoice }, { splChoice }].filter
+            (
+                item => Object.values(item)[0]
+            );
+        var genPassword = '';
 
-    // Array of Objects containing the Choices and then filtering out false choices
-    var choicesArr = [{ numChoice }, { lcChoice }, { ucChoice }, { splChoice }].filter
-      (
-        item => Object.values(item)[0]
-      );
-    //console.log(choicesArr); // Console logs the Array of Objects with their key of true
-
-    var genPassword = '';
-
-    // If no options/countChoices are selected, break out and advise user
-    if (countChoices === 0) {
-
-      password = "No options selected, You will need to select at least one! Please try again.";
-      return password;
-
+        // Randomly generates the characters per the end users confirmed choices using the genRanFunctions object
+        if (numChoice || lcChoice || ucChoice || splChoice) {
+            for (var i = 0; i <= pwdLength;) {
+                choicesArr.forEach(choice => {
+                    var chosen = Object.keys(choice)[0];
+                    genPassword += eval(genRanFunctions[chosen]);
+                    i++;
+                });
+            }
+            // This splits and randomly shuffles the generated genPassword string
+            password = genPassword.split('').sort(function () {
+                return 0.5 - Math.random()
+            }).join('');
+            return password.slice(0, pwdLength);
+        // Prompt end user no options/countChoices were selected
+        } else {
+            password = "No options selected, You will need to select at least one! Please try again.";
+            return password;
+        }
     } else {
-      // This loop use the genRanFunctions function to randomly generate the characters 
-      // per end users confirmed choice
-      for (var i = 0; i <= pwdLength;) {
-        choicesArr.forEach(choice => {
-          var chosen = Object.keys(choice)[0];
-          // console.log('choices: ', chosen);
-          genPassword += eval(genRanFunctions[chosen]);
-          i++;
-        });
-      }
-      // password = genPassword;
-      // This splits and randomly shuffles the genPassword string
-      password = genPassword.split('').sort(function () {
-        return 0.5 - Math.random()
-      }).join('');
-
-      // This slices the password and makes sure it matches the end user pwdlength input
-      // and then returns it to the password variable
-      return password.slice(0, pwdLength);
-
+        // Prompt end user of incorrect input number
+        password = "That number was not between 8 and 128! Please try again.";
+        return password;
     }
-
-  } else {
-    password = "That number was not between 8 and 128! Please try again.";
-    return password;
-  }
 }
